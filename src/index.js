1fc14595a6b9456d365efbf61eba5785fa0a6174
5hiV2anSh <piyushfulper3210@gmail.com>
2024-05-04 15:40:34 +0530

    Completed Task 5

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-const parseQuery = require('./queryParser');
-const readCSV = require('./csvReader');
+const parseQuery = require("./queryParser");
+const readCSV = require("./csvReader");
 
 async function executeSELECTQuery(query) {
-	const { fields, table } = parseQuery(query);
-	const data = await readCSV(`${table}.csv`);
+	const { fields, table, whereClause } = parseQuery(query);
+  const data = await readCSV(`${table}.csv`);
+  const filteredData = whereClause
+		? data.filter((row) => {
+			const [field, value] = whereClause.split("=").map((s) => s.trim());
+			return row[field] === value;
+		})
+		: data;
 
-	return data.map(row => {
-		const filteredRow = {};
-		fields.forEach(field => {
-			filteredRow[field] = row[field];
+    return filteredData.map((row) => {
+      const selectedRow = {};
+      fields.forEach((field) => {
+        selectedRow[field] = row[field];
 		});
-		return filteredRow;
+		return selectedRow;
 	});
 }
-
-module.exports = executeSELECTQuery;
